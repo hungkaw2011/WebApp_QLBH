ef50820dbb063ac0bdaa3b0652e480b94e6d67f1
Hung Nguyen <94224022+hungkaw2011@users.noreply.github.com>
2023-04-11 02:59:22 +0700

    Add files via upload

---
+++ Bài Tập Chung/Tuần 2/CSharp/CustomExe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCoban
+{
+    internal class CustomExe : Exception
+    {
+        const string str = "new Exp";
+        public CustomExe() : base(str) { }
+    }
+}
---
+++ Bài Tập Chung/Tuần 2/CSharp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCoban
+{
+    internal class Program
+    {
+        static int[] bubbleSort(ref int[] arr, int n, int p)
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = n - 1; j > i; j--)
+                {
+                    if (arr[j] * p > arr[j - 1] * p)
+                    {
+                        var tmp = arr[j];
+                        arr[j] = arr[j - 1];
+                        arr[j - 1] = tmp;
+                    }
+                }
+            }
+            return arr;
+        }
+        static void showArrBy(ref int[] arr, ArrayList<int> a, ArrayList<int> b, int n)
+        {
+            foreach (var item in arr)
+            {
+                a.Add(item % 2 == 0 ? item);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            Random rd = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rd.Next(10, 100);
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+            bubbleSort(ref arr, arr.Length, -1);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+            showArrBy(ref arr, 10);
+
+        }
+    }
+}

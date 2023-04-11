212b7161c2d9cef59f9350a2a3f0dea461488066
HungKaw <hungkaw@example.com>
2023-04-12 01:51:39 +0700

    commit all

--- WebApp/WebAppQLBH/Controllers/CategoryController.cs
+++ WebApp/WebAppQLBH/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Configuration;
 using System.Globalization;
 using System.Text.RegularExpressions;
 using WebAppQLBH.Data;
@@ -41,6 +42,60 @@ namespace WebAppQLBH.Controllers
             {
                 _db.Categories.Add(category);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";//lưu trữ tạm thời trên server, để lưu giữ thông tin giữa các Action hoặc Controller 
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+        //Get
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id==0)
+            {
+                return NotFound();
+            }
+            var category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return BadRequest();
+            }
+            return View(category);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            bool containsNumber = Regex.IsMatch(category.Name, @"\d");
+            if (containsNumber)
+            {
+                ModelState.AddModelError("Name", "Tên không được chứa chữ số!!!");
+            }
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Tên không được trùng lặp với số lượng đặt!!!");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _db.Categories.Find(id);
+            if (category != null) {
+                _db.Categories.Remove(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category deleted successfully";
                 return RedirectToAction("Index");
             }
             return View(category);
--- WebApp/WebAppQLBH/Models/Category.cs
+++ WebApp/WebAppQLBH/Models/Category.cs
@@ -8,8 +8,6 @@ namespace WebAppQLBH.Models
         [Key]
         public int Id { get; set; }
 
-        #pragma warning disable CS8618
-    // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         [Required]
         public string Name { get; set; }
 

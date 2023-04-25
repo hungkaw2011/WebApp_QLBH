4210c728a3b581305bd384c60d3637fcd76abe7c
HungKaw <hungkaw@example.com>
2023-04-25 14:49:21 +0700

    update

---
+++ Bài Tập Chung/Tuần 7/Bai12_Hung660_P1/Bai12_Hung660_P1/Models/HoaDon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Bai12_Hung660_P1.Models
+{
+    public partial class HoaDon
+    {
+        public string MaHd { get; set; }
+        public DateTime? NgayLap { get; set; }
+        public string MaKh { get; set; }
+        public string NguoiLap { get; set; }
+
+        public virtual KhachHang MaKhNavigation { get; set; }
+    }
+}
---
+++ Bài Tập Chung/Tuần 7/Bai12_Hung660_P1/Bai12_Hung660_P1/WindowLogin.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Bai12_Hung660_P1.Models;
+namespace Bai12_Hung660_P1
+{
+    /// <summary>
+    /// Interaction logic for WindowLogin.xaml
+    /// </summary>
+    public partial class WindowLogin : Window
+    {
+        public static string id;
+        public WindowLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            QLBanHangContext db = new QLBanHangContext();
+            var query = from account in db.Users
+                        select new
+                        {
+                            account.TenDn,
+                            account.MatKhau
+                        };
+            var txtId = idLogin.Text;
+            var pw = pwLogin.Text;
+            foreach (var item in query)
+            {
+                if (item.TenDn.CompareTo(txtId)==0 && item.MatKhau.CompareTo(pw) == 0)
+                {
+                    MainWindow main = new MainWindow();
+                    id = item.TenDn;
+                    main.Show();
+                    break;
+                }
+                else
+                {
+                    MessageBox.Show("Không tồn tại tài khoản này trong cơ sở dữ liệu", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                    break;
+                }
+            }
+        }
+    }
+}

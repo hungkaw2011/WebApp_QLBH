45f640a97d06b6059d727b63e9ab7be6b199d1ce
HungKaw <hungkaw@example.com>
2023-04-30 08:02:37 +0700

    datatale API

--- WebApp/WebApp.DataAccess/Repository/ProductRepository.cs
+++ WebApp/WebApp.DataAccess/Repository/ProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,9 +18,33 @@ namespace WebApp.DataAccess.Repository
             _db = db;
         }
 
+        /// <summary>
+        ///             Change Tracking
+        ///     Cơ chế giúp theo dõi sự thay đổi của từng object do Entity Framework đang quản lý trong bộ nhớ.
+        ///     Việc theo dõi giúp Entity Framework sinh ra những truy vấn phù hợp để thực thi trên cơ sở dữ liệu.
+        ///     Nhờ cơ chế này, việc cập nhật (thêm/sửa/xóa) object từ client code được đơn giản hóa
+        /// </summary>
+        /// <param name="obj"></param>
         public void Update(Product obj)
         {
-            _db.Products.Update(obj);
+            var productFromDb =_db.Products.FirstOrDefault(u=>u.Id== obj.Id);
+            //var productFromDb =_db.Products.AsNoTracking().FirstOrDefault(u=>u.Id== obj.Id);
+            if (productFromDb != null)
+            {
+                productFromDb.ISBN=obj.ISBN;
+                productFromDb.Price=obj.Price;
+                productFromDb.Author=obj.Author;
+                productFromDb.Description=obj.Description;
+                productFromDb.Title=obj.Title;
+                productFromDb.ListPrice = obj.ListPrice;
+                productFromDb.Price50=obj.Price50;
+                productFromDb.Price100=obj.Price100;
+
+                if (obj.ImageUrl!=null)
+                {
+                    productFromDb.ImageUrl = obj.ImageUrl;
+                }
+            }
         }
     }
 }
--- WebApp/WebAppQLBH/Areas/Admin/Controllers/ProductController.cs
+++ WebApp/WebAppQLBH/Areas/Admin/Controllers/ProductController.cs
@@ -14,21 +14,25 @@ namespace WebApp.Areas.Admin.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
 
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment; 
+        //lưu trữ thư mục chứa tài nguyên như wwwroot, appsettings.json, các biến môi trường và các thông tin khác như địa chỉ IP, port...
+
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment; 
+
         }
 
         public IActionResult Index()
         {
-            IEnumerable<Product> objProductList = _unitOfWork.Product.GetAll();
-            return View(objProductList);
+            return View();
         }
 
         //GET
+        [HttpGet]
         public IActionResult Upsert(int? id)
         {
-            //ViewBag.CategoryList = CategoryList;
 #pragma warning disable IDE0090 // Use 'new(...)'
             ProductVM productVM = new ProductVM()
             {
@@ -52,14 +56,41 @@ namespace WebApp.Areas.Admin.Controllers
                 return View(productVM);
             }
         }
-
-        //POST
+        /// <summary>
+        ///     String fileName=Guid.NewGuid().ToString()+Path.GetExtension(file.FileName);
+        ///     Tạo ra một chuỗi định danh duy nhất được sử dụng làm tên file
+        //      Ví dụ: Nếu file.FileName là "product_image.jpg", Guid.NewGuid().ToString()+Path.GetExtension(file.FileName)
+        //      Có thể trả về chuỗi "f8dd1a1b-6d9a-4f67-a8a8-246c2ee0d97a.jpg", là tên file mới được tạo ra để lưu trữ hình ảnh sản phẩm.
+        /// </summary>
+        /// <param name="productObj"></param>
+        /// <param name="file"></param>
+        /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Upsert(ProductVM productObj,IFormFile? file)
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid)//Kiểm tra tính hợp lệ của dữ liệu 
             {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;//Lấy đường dẫn đến thư mục gốc của website.
+                if (file != null)
+                {
+                    string fileName=Guid.NewGuid().ToString()+Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product"); //Tạo đường dẫn @wwwRootPath\images\product
+
+                    if (!string.IsNullOrEmpty(productObj.Product!.ImageUrl))
+                    {
+                        var oldImagePath = Path.Combine(wwwRootPath, productObj.Product.ImageUrl!.TrimStart('\\'));//Xóa kí tự \ đầu chuỗi cũ và nối lại 
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);//Xóa file cũ trong wwwroot\images\product
+                        }
+                    }
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);//Thêm tệp vào fileStream
+                    }
+                    productObj.Product!.ImageUrl = @"\images\product\" + fileName;
+                }
                 if (productObj.Product!.Id == 0)
                 {
                     _unitOfWork.Product.Add(productObj.Product);
@@ -120,5 +151,12 @@ namespace WebApp.Areas.Admin.Controllers
             return RedirectToAction("Index");
 
         }
-    }
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll() { 
+            var objProductList = _unitOfWork.Product.GetAll(includeProperties:"Category");
+            return Json(new {data= objProductList});
+        }
+        #endregion
+}
 }

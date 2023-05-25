9b6d80927b3bd0380145d6de99f7b5c2555624f2
Hùng Nguyễn <hungqaz38@gmail.com>
2023-05-25 08:24:47 +0700

    Company Crud

--- WebApp/WebAppQLBH/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ WebApp/WebAppQLBH/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -94,10 +94,6 @@ namespace WebAppQLBH.Areas.Identity.Pages.Account
             [Display(Name = "Password")]
             public string Password { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
